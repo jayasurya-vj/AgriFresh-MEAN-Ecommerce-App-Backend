88c147ad70955b84f8bec94e7b2dc7b8d1f7cf66
jayasurya-vj <jayasurya.vjeee21@gmail.com>
2022-08-29 14:47:59 +0530

    redirect changes

--- server.js
+++ server.js
@@ -20,13 +20,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use("/",express.static("angular"));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin","*");
-  res.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,Authorization,X-Content-Type-Options ");
+  res.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,Authorization,X-Content-Type-Options,Access-Control-Allow-Origin");
   res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,DELETE,OPTIONS,PUT");
   next();
 })
+
+app.use("/",express.static("angular"));
 
 app.get("/test",(req,res)=>{
   res.send("test");
