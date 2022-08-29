3d6d21d2cad1af75cf51326871540d6dcd64f0b1
jayasurya-vj <jayasurya.vjeee21@gmail.com>
2022-08-29 14:55:21 +0530

    redirect changes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/api/cart",cartRouter);
 app.use("/api/order",orderRouter);
 app.use("/api/payment",paymentRouter);
 app.use("",(req,res,next)=>{
-  const indexFile = path.resolve(__dirname + '/angular/index.html');
+  const indexFile = path.join(__dirname + '/angular/index.html');
   res.sendFile(indexFile);
   // res.sendFile(path.join(__dirname,"\angular","\index.html"));
 });
